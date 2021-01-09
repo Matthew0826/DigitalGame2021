0377a7b656c1930f91738926c861c91bda654340
Matthew G <doubleeclipse8264@gmail.com>
2021-01-09 17:29:18 -0500

    Added Entity.java

---
+++ src/Entities/Entity.java
@@ -0,0 +1,66 @@
+package Entities;
+
+import java.awt.Image;
+
+public abstract class Entity {
+	
+	private int x;
+	private int y;
+	
+	private int dx;
+	private int dy;
+	
+	private Image image;
+	
+	public Entity( int x, int y, Image image ){
+		this.x = x;
+		this.y = y;
+		this.image = image;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	
+	
+	public void setX( int x ) {
+		this.x = x;
+	}
+	public void setY( int y ) {
+		this.y = y;
+	}
+	
+	
+	
+	public int getDx() {
+		return dx;
+	}
+	public int getDy() {
+		return dy;
+	}
+	
+	public void setDx( int dx ) {
+		this.dx = dx;
+	}
+	public void setDy( int dy ) {
+		this.dy = dy;
+	}
+	
+	public void move() {
+		x += dx;
+		y += dy;
+	}
+	
+	
+	public Image getImage() {
+		return image;
+	}
+	
+	public void setImage( Image i ) {
+		image = i;
+	}
+	
+}
