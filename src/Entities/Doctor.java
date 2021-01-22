f915226736b04f8464579c47d8eef5fcfd25ab21
Daniel <Daniel@DanielPC>
2021-01-22 13:33:26 -0500

    Did it work?

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -6,6 +6,21 @@ public class Doctor extends Enemy{
 	
 	public static final String imageString = "images/entites/doctor/doctor0.png";
 	public static final ImageIcon startingImage = new ImageIcon( imageString );
+	
+	private int collectedMoney;
+	
+	public int getCollectedMoney() {
+		return collectedMoney;
+	}
+	
+	public int setCollectedMoney(int c) {
+		return collectedMoney + c;
+	}
+	
+	
+	public void trackHarmedEnemy(Eneny enemy) {
+		//I think?
+	}
 	    
 	public Doctor( int x, int y ){
 		super( x, y, startingImage.getImage() );
