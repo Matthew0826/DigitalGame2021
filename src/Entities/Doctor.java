dbe41d45b91247adf3ff7f7ef0c242106fc9b759
SquidwardSquad <65752886+SquidwardSquad@users.noreply.github.com>
2021-02-12 13:41:50 -0500

    Update Doctor.java

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -7,6 +7,12 @@ public class Doctor extends Enemy{
 	public static final String imageString = "images/entites/doctor/doctor0.png";
 	public static final ImageIcon startingImage = new ImageIcon( imageString );
 	
+	public Doctor( int x, int y ){
+		super( x, y, startingImage.getImage() );
+		setWidth(200);
+		setHeight(400);
+	}
+	
 	private int collectedMoney;
 	
 	public int getCollectedMoney() {
@@ -21,9 +27,6 @@ public class Doctor extends Enemy{
 		//I think?
 	}
 	    
-	public Doctor( int x, int y ){
-		super( x, y, startingImage.getImage() );
-	}
 
 	@Override
 	public void trackAlly(Ally ally) {
